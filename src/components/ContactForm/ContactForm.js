44ae3cd51b60fa236b8e29d03a321a753cf309a0
Mateusz Blyszcz <mateusz.blyszcz@tsh.io>
2018-08-29 14:15:41 +0200

    Init commit

---
+++ src/components/ContactForm/ContactForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class ContactForm extends Component {
+  render() {
+    return (
+      <div>
+        <h1>ContactForm</h1>
+      </div>
+    )
+  }
+}
+
+export default ContactForm
