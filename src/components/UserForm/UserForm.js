fe030bb1f25198cfde383e60a91ba4794c97d059
Mateusz Blyszcz <mateusz.blyszcz@tsh.io>
2018-09-07 11:40:45 +0200

    Extending PureComponent instead of Component

--- src/components/UserForm/UserForm.js
+++ src/components/UserForm/UserForm.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import { reduxForm, Field } from 'redux-form';
 import { validateAsync } from './validateAsync';
 
@@ -14,7 +14,7 @@ const renderInput = ({ input, type, label, meta: { touched, error, warning }}) =
   )
 }
 
-export class UserForm extends Component {
+export class UserForm extends PureComponent {
   render() {
     return (
       <form onSubmit={this.props.handleSubmit(validateAsync)} className="container needs-validation">
